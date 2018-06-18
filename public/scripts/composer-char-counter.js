e7ddf32448fa7417528f4473a45a58c183846165
jonathan <usixjad@gmail.com>
2018-06-18 21:38:03 +0000

    counter function implemented

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,9 @@
-$(document).ready(() => {
-  var counter = $('.counter');
-  counter.on('keyup', () => {
-    
+$(document).ready(function() {
+
+  $('#user-input').on('keyup', function (e) {
+    var currentLength = 0;
+    currentLength = e.target.value.length;
+    $(this).siblings('.counter').text(140 - currentLength);
+    // alternatively: $(".counter").text(140 - currentLength);
   })
 });
